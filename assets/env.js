f51a62b2cd2f8f5e66fafa1ffb57f21ed334a389
empulseproject <112696571+empulseproject@users.noreply.github.com>
2022-09-08 22:20:25 +0530

    Add files via upload

---
+++ assets/env.js
@@ -0,0 +1,38 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+ 
+  // window.__env.apiUrl = 'http://localhost:55873/api/';
+  // window.__env.apiUrl = 'http://10.1.1.16/QADemoAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/IAHSAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/UDPAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/UDPDemoAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/UDPNewAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/NVHEAMAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/NVHETMAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/EmSalesAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/UDPTestAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/IndoAmericanAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/EmSalesPSLAPI/api/';
+  // window.__env.apiUrl = 'http://106.51.81.198/TaskAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/SmyrnaMSAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/QTCEAMAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/SmyrnaMSDemoAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/ProcessImprovementAPI/api/';
+  // window.__env.apiUrl = 'http://106.51.81.198:2002/eMpulseTaskAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/NVHEAMWAAPI/api/';
+  // window.__env.apiUrl = 'http://106.51.81.198:2009/UDPNewAPI/api/';
+  // window.__env.apiUrl = 'http://106.51.81.198:2010/IAHSAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/FireQCAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/RugbyAPI/api/';
+  // window.__env.apiUrl = ' http://106.51.81.198:2002/eMpulseTaskAPI/api/';
+  window.__env.apiUrl = 'https://www.empulseudp.com/SmyrnaMSV2API/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/EmpulseEAMAPI/api/';
+  // window.__env.apiUrl = 'https://www.empulseudp.com/QTCEAMLAPI/api/';
+  
+// 
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = false;
+}(this));
